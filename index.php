53ada861af9167226059877589c5f23a58cfe429
Otibaco <orjifranklin42@gmail.com>
2026-08-14 11:35:34 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <!-- <meta http-equiv="refresh" content="1"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+
+<body>
+
+    <form action="pages/about.php" method="post">
+
+        <label for="nameinput">Name:</label><br>
+        <input type="text" name="name" id="nameinput"><br>
+
+        <label for="emailinput"> E-mail:</label><br>
+        <input type="email" name="email" id="emailinput"><br>
+        <input type="submit">
+    </form>
+
+
+
+
+</body>
+
+</html>
